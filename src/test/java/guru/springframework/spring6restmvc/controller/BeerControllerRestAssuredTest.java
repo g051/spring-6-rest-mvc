808b37303e4c2a4352543496462b9b391217aee4
Yadong <guo.yadong@gt.tech.gov.sg>
2024-12-30 23:07:01 +0800

    Import REST assured and Swagger validator

---
+++ src/test/java/guru/springframework/spring6restmvc/controller/BeerControllerRestAssuredTest.java
@@ -0,0 +1,58 @@
+
+package guru.springframework.spring6restmvc.controller;
+
+import static io.restassured.RestAssured.given;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.test.context.ActiveProfiles;
+
+/**
+ * Created by jt, Spring Framework Guru.
+ */
+@ActiveProfiles("test")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Import(BeerControllerRestAssuredTest.TestConfig.class)
+@ComponentScan(basePackages = "guru.springframework.spring6restmvc")
+public class BeerControllerRestAssuredTest {
+
+  @Configuration
+  public static class TestConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+      http.authorizeHttpRequests(authorize -> {
+        authorize.anyRequest().permitAll();
+      });
+
+      return http.build();
+    }
+  }
+
+  @LocalServerPort
+  Integer localPort;
+
+  @BeforeEach
+  void setUp() {
+    RestAssured.baseURI = "http://localhost";
+    RestAssured.port = localPort;
+  }
+
+  @Test
+  void testListBeers() {
+    given().contentType(ContentType.JSON)
+        .when()
+        .get("/api/v1/beer")
+        .then()
+        .assertThat().statusCode(200);
+  }
+}
