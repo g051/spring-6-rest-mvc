690a89aee83b1105f38c6fb9d6b11c26d1b7a4e7
Yadong <guo.yadong@gt.tech.gov.sg>
2024-12-30 16:34:59 +0800

    Fix swagger-ui.html issue

--- src/main/java/guru/springframework/spring6restmvc/config/SpringSecurityConfig.java
+++ src/main/java/guru/springframework/spring6restmvc/config/SpringSecurityConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+//import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 /**
@@ -17,7 +17,7 @@ public class SpringSecurityConfig {
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
     http.authorizeHttpRequests(authorize -> authorize
-            .requestMatchers("/v3/api-docs**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
+            .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-resources/*", "/swagger-ui.html").permitAll()
             .anyRequest().authenticated())
 //        .httpBasic(Customizer.withDefaults())
 //        .csrf(CsrfConfigurer -> CsrfConfigurer.ignoringRequestMatchers("/api/**"));
